cae1291212442199b97723b9ac6186f8422c0b72
Drew Maughan <drew.maughan@toptal.com>
2025-03-20 16:01:47 +0000

    feat: setting up acts.

---
+++ database/factories/ActFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Act;
+use App\Models\ActProfile;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Model>
+ */
+class ActFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->name();
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name)
+        ];
+    }
+
+    public function withProfile(): Factory|ActFactory
+    {
+        return $this->afterCreating(fn(Act $act) => ActProfile::factory()->createOne([
+            'act_id' => $act->id,
+        ])
+        );
+    }
+}
