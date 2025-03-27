4b3b0d0173f69a00b200b72c23bc46b262c8ce76
Drew Maughan <drew.maughan@toptal.com>
2025-03-27 16:19:58 +0000

    feat: adding Round::result(), which produces a ranked order of results.

--- app/Models/Round.php
+++ app/Models/Round.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Database\Factories\RoundFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -41,4 +42,64 @@ class Round extends Model
         return $this->hasMany(RoundVote::class);
     }
 
+    /**
+     * Returns the results of this Round in ranked order.
+     *
+     * @return Collection|null
+     */
+    public function results(): ?Collection
+    {
+        if ($this->outcomes()->count())
+        {
+            return $this->outcomes()->get()
+                        ->sort(fn($a, $b) => self::sortOutcomes($a, $b))
+                        ->values(); // a nasty gotcha - otherwise the keys are preserved.
+        }
+        return null;
+    }
+
+    /**
+     * A callback function for sorting RoundOutcomes.
+     * The entries are ranked in descending order, by:
+     * - total score;
+     * - number of first choice votes;
+     * - number of second choice votes.
+     *
+     * @param RoundOutcome $a
+     * @param RoundOutcome $b
+     * @return int
+     */
+    protected function sortOutcomes(RoundOutcome $a, RoundOutcome $b): int
+    {
+        $score_result         = $this->intcmp($a->score, $b->score);
+        $first_choice_result  = $this->intcmp($a->first_votes, $b->first_votes);
+        $second_choice_result = $this->intcmp($a->second_votes, $b->second_votes);
+
+        if ($score_result) {
+            return $score_result;
+        }
+        if ($first_choice_result) {
+            return $first_choice_result;
+        }
+        return $second_choice_result;
+    }
+
+    /**
+     * A convenience function for returning a value for sorting based on two integers.
+     *
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
+    protected function intcmp(int $a, int $b): int
+    {
+        if ($a === $b)
+        {
+            return 0;
+        }
+        else
+        {
+            return $a < $b ? 1 : -1;
+        }
+    }
 }
