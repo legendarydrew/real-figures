3298a14c4aa24411cd5cc4dec14500f6984ebe7f
Drew Maughan <drew.maughan@toptal.com>
2025-04-16 14:21:38 +0100

    feat: the ability to sort songs by each column.

--- app/Http/Controllers/Back/SongsController.php
+++ app/Http/Controllers/Back/SongsController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\Back;
 use App\Http\Controllers\Controller;
 use App\Models\Act;
 use App\Models\Song;
-use App\Models\Stage;
 use App\Transformers\SongAdminTransformer;
-use App\Transformers\StageAdminTransformer;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -16,9 +14,16 @@ class SongsController extends Controller
 
     public function index(): Response
     {
+        // Sorting rows is done by passing a sort parameter, in the format "column:asc" or "column:desc".
+        $sort = explode(':', request()->query('sort', 'title:asc'));
+
         return Inertia::render('back/songs', [
-            'acts' => Act::select(['id', 'name'])->orderBy('name')->get(),
-            'songs' => fractal(Song::paginate())->transformWith(SongAdminTransformer::class)->toArray()
+            'acts'  => fn() => Act::select(['id', 'name'])->orderBy('name')->get(),
+            'songs' => fn() => fractal(
+            // https://stackoverflow.com/questions/38261546/order-by-relationship-column#38262311
+                Song::join('acts', 'act_id', '=', 'acts.id')
+                    ->orderBy(...$sort)->paginate()
+            )->transformWith(SongAdminTransformer::class)->toArray()
         ]);
     }
 }
--- resources/js/Pages/back/songs.tsx
+++ resources/js/Pages/back/songs.tsx
@@ -1,26 +1,51 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head, router } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
-import React, { useState } from 'react';
-import { Edit, Trash } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { ChevronDown, ChevronUp, Edit, Trash } from 'lucide-react';
 import { Song } from '@/types';
 import { SongDialog } from '@/components/admin/song-dialog';
 import { DeleteSongDialog } from '@/components/admin/delete-song-dialog';
 import { LanguageFlag } from '@/components/language-flag';
 import { Pagination } from '@/components/admin/pagination';
+import { Icon } from '@/components/icon';
 
-// TODO sort songs.
 // TODO filter songs.
+
+interface TableSort {
+    column: string;
+    asc: boolean;
+}
 
 export default function Songs({ acts, songs }: Readonly<{ stages: Song[] }>) {
 
     const [currentSong, setCurrentSong] = useState<Song>();
+    const [sort, setSort] = useState<TableSort>({ column: 'title', asc: true });
     const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false);
     const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<boolean>(false);
 
     const pageHandler = (pageNumber: number): void => {
-        router.reload({ data: { page: pageNumber } });
+        fetchSongs(pageNumber);
     };
+
+    const sortHandler = (column: string): void => {
+        setSort({ column, asc: column === sort.column ? !sort.asc : sort.asc });
+    };
+
+    const fetchSongs = (pageNumber: number = 1): void => {
+        router.reload({
+            data: {
+                page: pageNumber ?? songs.meta.pagination.current_page,
+                sort: `${sort.column}:${sort.asc ? 'asc' : 'desc'}`
+            },
+            only: ['songs'],
+            showProgress: true
+        });
+    }
+
+    useEffect(() => {
+        fetchSongs();
+    }, [sort]);
 
     const editHandler = (stage: Song): void => {
         setCurrentSong(stage);
@@ -47,9 +72,24 @@ export default function Songs({ acts, songs }: Readonly<{ stages: Song[] }>) {
                 <thead className="text-sm">
                 <tr className="border-b-2">
                     <th/>
-                    <th scope="col" className="text-left px-2">Title</th>
-                    <th scope="col" className="text-left px-2">Act</th>
-                    <th scope="col" className="text-right px-2">Play count</th>
+                    <th scope="col" className="text-left px-2 select-none cursor-pointer"
+                        onClick={() => sortHandler('title')}>
+                        Title
+                        {sort.column === 'title' ? (
+                            <Icon iconNode={sort.asc ? ChevronUp : ChevronDown} className="h-3 inline"/>) : ''}
+                    </th>
+                    <th scope="col" className="text-left px-2 select-none cursor-pointer"
+                        onClick={() => sortHandler('acts.name')}>
+                        Act
+                        {sort.column === 'acts.name' ? (
+                            <Icon iconNode={sort.asc ? ChevronUp : ChevronDown} className="h-3 inline"/>) : ''}
+                    </th>
+                    <th scope="col" className="text-right px-2 select-none cursor-pointer"
+                        onClick={() => sortHandler('play_count')}>
+                        Play count
+                        {sort.column === 'play_count' ? (
+                            <Icon iconNode={sort.asc ? ChevronUp : ChevronDown} className="h-3 inline"/>) : ''}
+                    </th>
                     <th/>
                 </tr>
                 </thead>
