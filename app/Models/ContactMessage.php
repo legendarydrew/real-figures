128984eb2b4b83c78a467c8aa76a2858db915b6e
Drew Maughan <drew.maughan@toptal.com>
2025-04-17 18:04:13 +0100

    feat: creating a new table for contact messages.

---
+++ app/Models/ContactMessage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ContactMessageFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactMessage extends Model
+{
+    /** @use HasFactory<ContactMessageFactory> */
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+}
---
+++ database/factories/ContactMessageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactMessage>
+ */
+class ContactMessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'          => $this->faker->name(),
+            'email'         => $this->faker->unique()->safeEmail(),
+            'body'          => $this->faker->realText(),
+            'ip'            => $this->faker->ipv4(),
+            'captcha_score' => $this->faker->biasedNumberBetween(1, 10) / 10,
+        ];
+    }
+}
