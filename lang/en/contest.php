b12489b7927f2c10cdb0ba9a34fb71269f2b19f9
Drew Maughan <drew.maughan@toptal.com>
2025-04-22 15:41:26 +0100

    feat: added a status property to the Stage model, with test.
    Began creating a language file.

---
+++ lang/en/contest.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'stage' => [
+        'status' => [
+            'inactive'  => 'Inactive',
+            'ready'     => 'Ready',
+            'started'   => 'Started',
+            'judgement' => 'Judgement',
+            'ended'     => 'Ended',
+        ]
+    ]
+];
