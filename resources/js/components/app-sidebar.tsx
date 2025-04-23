e981a8998d0374127faaba6c645e93c39eaf7c59
Drew Maughan <drew.maughan@toptal.com>
2025-04-23 12:49:13 +0100

    refactor: creating a Dashboard admin controller.

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -18,7 +18,7 @@ import AppLogo from './app-logo';
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
-        href: '/dashboard',
+        href: '/admin',
         icon: LayoutGrid
     },
     {
