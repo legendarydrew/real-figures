33a63a36cb1359d5d5af827cbb99919ac25133bf
Drew Maughan <drew.maughan@toptal.com>
2025-04-24 12:03:23 +0100

    feat: smaller chart labels.

--- resources/js/components/admin/dashboard-song-total-plays.tsx
+++ resources/js/components/admin/dashboard-song-total-plays.tsx
@@ -15,14 +15,19 @@ interface DashboardSongPlaysProps {
 
 export const DashboardSongTotalPlays: React.FC<DashboardSongPlaysProps> = ({ data }) => {
 
+    const labelStyle = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    };
+
     return (
         <section>
             <h2 className="font-bold mb-2">Song Plays <small>in the last week</small></h2>
-            <ResponsiveContainer className="w-full h-[12rem]" aspect={3}>
+            <ResponsiveContainer className="w-full h-[12rem]" aspect={2.5}>
                 <LineChart data={data.days}>
                     <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="date" type="category"/>
-                    <YAxis/>
+                    <XAxis dataKey="date" type="category" tickCount={7} style={labelStyle} padding={{ top: 8 }}/>
+                    <YAxis style={labelStyle}/>
                     <Line dataKey="play_count" label="Total song plays"/>
                 </LineChart>
             </ResponsiveContainer>
