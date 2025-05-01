72d2a4010ef8cb055e543864ff9cd25a36bffca6
Drew Maughan <drew.maughan@toptal.com>
2025-05-01 16:13:14 +0100

    feat: adding another link to the home page in the header.

--- resources/js/components/front/front-header.tsx
+++ resources/js/components/front/front-header.tsx
@@ -17,6 +17,7 @@ export const FrontHeader: React.FC = () => {
                 </Link>
 
                 <nav className="flex items-center justify-end gap-4">
+                    <Link href={route('home')} className={linkStyle}>Contest</Link>
                     <Link href={route('rules')} className={linkStyle}>Rules</Link>
                     <Link href={route('about')} className={linkStyle}>About</Link>
                     {auth.user ? (
