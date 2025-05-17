a48400009ead3e2d823921211ec4e5d3d8574d32
Drew Maughan <drew.maughan@toptal.com>
2025-05-17 17:45:55 +0100

    feat: updating About content.

--- resources/js/Pages/front/about.tsx
+++ resources/js/Pages/front/about.tsx
@@ -28,29 +28,48 @@ const AboutPage: React.FC = () => {
             <FrontContent>
                 <Heading title="Real Figures Don't F.O.L.D &ndash; About the Project"/>
 
-                <div className="flex flex-col lg:flex-row mb-5 gap-10">
-                    <div className="lg:w-3/5 content">
-                        <h2 className="text-base font-semibold">
-                            The motivations behind creating this site were:
-                        </h2>
-                        <ul>
-                            <li><b className="text-current">Revisiting one of SilentMode's earliest
-                                Creations:</b> CATAWOL Records began life as a modular building.
+                <div className="content pt-3 pb-5 lg:pb-10 lg:px-2 flex flex-col md:flex-row gap-5 lg:gap-10">
+                    <PlaceholderPattern className="md:w-2/5 stroke-neutral-900/20"/>
+                    <div className="content md:w-3/5">
+                        <p className="text-lg">
+                            <b>Real Figures Don't F.O.L.D combines SilentMode's interest in LEGO</b> with music,
+                            "artificial intelligence", web development and advocacy.
+                        </p>
+                        <p>This project serves many purposes:</p>
+
+                        <ul className="list-disc">
+                            <li>
+                                <HeadingSmall title="Revisiting one of SilentMode's earliest Creations."/>
+                                <p className="mb-3">CATAWOL Records began life as a modular building, designed and built
+                                    near the beginning of SilentMode's time in the LEGO hobby. The model was designed
+                                    without ever having owned or built an official modular building set.</p>
                             </li>
-                            <li><b className="text-current">Embarking on an ambitious [loosely-related] LEGO
-                                project:</b> SilentMode's first one to fully incorporate music, as well as
-                                computer-generated content.
+                            <li>
+                                <HeadingSmall title="Embarking on an ambitious LEGO project."/>
+                                <p className="mb-3">Expanding on his existing skills as a Maker, Artist and LEGO
+                                    Enthusiast, this is SilentMode's first project to fully incorporate music, as well
+                                    as AI/computer-generated content.</p>
                             </li>
-                            <li><b className="text-current">Creating the <i>first ever anti-bullying campaign</i> (that
-                                we know of) within the LEGO space.</b></li>
-                            <li><b className="text-current">A live demonstration of coding ability:</b> built
-                                single-handedly from scratch, proving that SilentMode <i>can</i> code.
+                            <li>
+                                <HeadingSmall
+                                    title="Creating the first ever anti-bullying campaign (that we know of) within the LEGO space."/>
+                                <p className="mb-3">An opportunity for SilentMode to highlight an important issue,
+                                    which affects <em>both children and adults</em>, that has never been
+                                    addressed before in the context of LEGO.</p>
+                            </li>
+                            <li>
+                                <HeadingSmall title="A live demonstration of coding ability."/>
+                                <p className="mb-3">This site was designed and built by SilentMode himself, using
+                                    Laravel and Inertia for the back end, and React with Tailwind for the front end.
+                                    Hopefully it will help him land his next role.</p>
                             </li>
                         </ul>
+
                     </div>
-                    <div className="lg:w-2/5 max-h-[20rem]">
-                        <Advert/>
-                    </div>
+                </div>
+
+                <div className="mx-auto">
+                    <Advert/>
                 </div>
 
                 <Collapsible className="border-b" open={isOpen('about-label')}
--- resources/js/components/front/about-banner.tsx
+++ resources/js/components/front/about-banner.tsx
@@ -1,14 +1,15 @@
 import Heading from '@/components/heading';
 import { Button } from '@/components/ui/button';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-import { Advert } from '@/components/advert';
 import { Link, router } from '@inertiajs/react';
 import { SubscribePanel } from '@/components/front/subscribe-panel';
 
 export default function AboutBanner() {
 
     const moreInfoHandler = () => {
-        router.visit('about');
+        router.visit('about', {
+            preserveScroll: false
+        });
     };
 
     return (
@@ -16,11 +17,14 @@ export default function AboutBanner() {
             <div className="max-w-5xl mx-auto grid gap-4 grid-cols-1 md:grid-cols-2">
 
                 <div className="pr-10">
+                    <PlaceholderPattern
+                        className="w-full h-[10rem] mb-5 stroke-neutral-900/20 dark:stroke-neutral-100/20"/>
+
                     <Heading title="About the Contest"/>
-                    <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-                        delectus dolorum expedita facere fugit illo in neque non perferendis, possimus quasi repellendus
-                        repudiandae soluta
-                        sunt, suscipit, vel velit veritatis voluptatum?</p>
+                    <p className="mb-3">
+                        With our roster of talented Acts, CATAWOL Records and the MODE Family join forces
+                        to raise awareness of bullying within hobby spaces, through the power of music.
+                    </p>
                     <Button type="button" size="lg" className="cursor-pointer" onClick={moreInfoHandler}>More
                         information</Button>
                 </div>
@@ -28,14 +32,19 @@ export default function AboutBanner() {
                 <div className="flex flex-col gap-3 md:grid md:gap-2 md:grid-cols-2 md:grid-rows-2">
                     <SubscribePanel className="w-full col-span-1 md:col-span-2"/>
 
-                    <div className="bg-green-500 p-3 col-span-1 h-10 md:h-auto rounded-sm">
-                        <Link href={route('donations')}>Donor Wall</Link>
-                    </div>
+                    <Link
+                        className="flex flex-col justify-end bg-green-700 hover:bg-green-800 text-white font-semibold p-3 col-span-1 h-10 md:h-auto rounded-sm leading-none"
+                        href={route('donations')}>
+                        Donor Wall
+                    </Link>
 
-                    <PlaceholderPattern
-                        className="w-full col-span-1 stroke-neutral-900/20 dark:stroke-neutral-100/20"/>
-
-                    <Advert className="w-full h-[12rem] col-span-2"/>
+                    <Link
+                        className="flex flex-col justify-end bg-red-500 hover:bg-red-600 text-white font-semibold p-3 col-span-1 h-10 md:h-auto rounded-sm leading-none"
+                        href="https://youtube.com/@silentmodetv" target="_blank">
+                        SilentMode
+                        <br/>
+                        <span className="text-sm">on YouTube</span>
+                    </Link>
                 </div>
 
             </div>
