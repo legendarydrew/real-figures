4807a767c2d74308bda0ef1c34d57b93c40a8dcd
Drew Maughan <drew.maughan@toptal.com>
2025-05-17 09:09:02 +0100

    feat: making the contest header responsive.

--- resources/js/components/front/contest-header.tsx
+++ resources/js/components/front/contest-header.tsx
@@ -2,16 +2,16 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 
 export default function ContestHeader() {
     return (
-        <div className="bg-indigo-200 py-10 px-5">
-            <div className="max-w-5xl mx-auto flex items-center gap-4">
+        <div className="bg-indigo-200 md:py-10 md:px-5">
+            <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-4">
 
                 <PlaceholderPattern className="stroke-neutral-900/20 dark:stroke-neutral-100/20"/>
 
-                <div className="w-1/2 ml-10">
+                <div className="px-3 md:px-0 md:w-1/2 md:ml-10">
                     <h1 className="display-text text-4xl text-shadow-md mb-3">
                         32 Acts.<br/>1 Anthem.
                     </h1>
-                    <p className="text-base w-3/4">
+                    <p className="text-base mb-3 md:w-3/4">
                         We're raising awareness about bullying through music - and <b>you</b> help pick the
                         winner!
                     </p>
