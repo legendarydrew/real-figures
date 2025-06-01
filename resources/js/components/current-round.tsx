93cf02783b69a6a04649a826f5283946619032c5
Drew Maughan <drew.maughan@toptal.com>
2025-06-01 12:52:44 +0100

    feat: creating a Golden Buzzer dialog.
    Information is passed to the dialog through a hook.

--- resources/js/components/current-round.tsx
+++ resources/js/components/current-round.tsx
@@ -1,10 +1,11 @@
 import { ActImage } from '@/components/ui/act-image';
-import { Round } from '@/types';
+import { Round, Song } from '@/types';
 import { LanguageFlag } from '@/components/language-flag';
 import { ROUND_VOTE_DIALOG_NAME, RoundVoteDialog } from '@/components/front/round-vote-dialog';
 import { Button } from '@/components/ui/button';
 import { useDialog } from '@/context/dialog-context';
 import { StarIcon } from 'lucide-react';
+import { GOLDEN_BUZZER_DIALOG_NAME } from '@/components/front/golden-buzzer-dialog';
 
 interface CurrentRoundProps {
     round?: Round;
@@ -23,6 +24,10 @@ export const CurrentRound: React.FC<CurrentRoundProps> = ({ round }) => {
         openDialog(ROUND_VOTE_DIALOG_NAME);
     };
 
+    const beginBuzzerHandler = (song: Song): void => {
+        openDialog(GOLDEN_BUZZER_DIALOG_NAME, { stage: round?.id, song });
+    };
+
     return (
         round ? (
             <>
@@ -34,21 +39,24 @@ export const CurrentRound: React.FC<CurrentRoundProps> = ({ round }) => {
                                 <ActImage act={song.act} size="full"/>
                                 <div className="p-3 lg:p-5 absolute bottom-0 w-full">
                                     <div className="text-lg font-semibold leading-tight">{song.act.name}</div>
-                                    <div className="flex items-center truncate gap-2 text-sm font-semibold leading-tight">
+                                    <div
+                                        className="flex items-center truncate gap-2 text-sm font-semibold leading-tight">
                                         <LanguageFlag languageCode={song.language}/>
                                         {song.title}
                                     </div>
                                 </div>
                             </div>
-                            <Button className="w-full rounded-t-none uppercase" variant="gold" size="lg" type="button">
-                                <StarIcon /> Golden Buzzer <StarIcon />
+                            <Button className="w-full rounded-t-none uppercase" variant="gold" size="lg" type="button"
+                                    onClick={() => beginBuzzerHandler(song)}>
+                                <StarIcon/> Golden Buzzer <StarIcon/>
                             </Button>
                         </li>
                     ))}
                 </ul>
 
                 {/* A big button for casting a vote. */}
-                <Button size="lg" className="w-full my-3 text-base" type="button" onClick={beginVoteHandler}>Cast your Vote...</Button>
+                <Button size="lg" className="w-full my-3 text-base" type="button" onClick={beginVoteHandler}>Cast your
+                    Vote...</Button>
                 <RoundVoteDialog round={round}/>
             </>
         ) : ''
--- resources/js/components/front/golden-buzzer-dialog.tsx
+++ resources/js/components/front/golden-buzzer-dialog.tsx
@@ -1,9 +1,9 @@
 import { ChangeEvent, FC, useEffect, useState } from 'react';
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogFooter, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { Link, usePage } from '@inertiajs/react';
+import { usePage } from '@inertiajs/react';
 import { PaypalButton } from '@/components/paypal-button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Textarea } from '@/components/ui/textarea';
@@ -11,23 +11,32 @@ import { Alert } from '@/components/alert';
 import { useDialog } from '@/context/dialog-context';
 import { useAnalytics } from '@/hooks/use-analytics';
 import ConfettiExplosion, { ConfettiProps } from 'react-confetti-explosion';
+import { Act, Round, Song } from '@/types';
+import { ActImage } from '@/components/ui/act-image';
+import { LanguageFlag } from '@/components/language-flag';
 
-interface DonateDialogProps {
+interface GoldenBuzzerDialogProps {
     // Dialog properties.
+    // This dialog has to be opened from more than one place, and we want to be able to set
+    // the respective Round and Act.
+    round: Round;
+    act: Act;
     open: boolean;
     onOpenChange: () => void;
 }
 
-export const DONATE_DIALOG_NAME = 'donate';
+export const GOLDEN_BUZZER_DIALOG_NAME = 'golden-buzzer';
 
-export const DonateDialog: FC<DonateDialogProps> = () => {
+export const GoldenBuzzerDialog: FC<GoldenBuzzerDialogProps> = () => {
 
-    const { openDialogName, closeDialog } = useDialog();
-    const isOpen = openDialogName === DONATE_DIALOG_NAME;
+    const { openDialogName, closeDialog, dialogProps } = useDialog();
+    const isOpen = openDialogName === GOLDEN_BUZZER_DIALOG_NAME;
 
     const { donation } = usePage().props;
     const { trackEvent } = useAnalytics();
 
+    const [round, setRound] = useState<Round>();
+    const [song, setSong] = useState<Song>();
     const [amount, setAmount] = useState<number>(donation.default.general);
     const [message, setMessage] = useState<string>('');
     const [isAnonymous, setIsAnonymous] = useState<boolean>(false);
@@ -36,17 +45,26 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
 
     const confettiSettings: ConfettiProps = {
         force: 0.9,
-        duration: 5000,
+        duration: 8000,
         particleCount: 250,
         width: window.innerWidth,
         colors: [
-            '#0B6623',
-            '#29AB87',
-            '#50C878',
-            '#B2EC5D'
+            '#ffd700',
+            '#eee8aa',
+            '#daa520',
+            '#da9100',
+            '#fcc200',
+            '#996515'
         ],
         zIndex: 100
     };
+
+    useEffect(() => {
+        if (isOpen) {
+            setRound(dialogProps.round);
+            setSong(dialogProps.song);
+        }
+    }, [isOpen]);
 
     const amountHandler = (e: ChangeEvent): void => {
         setAmount(parseFloat(e.target.value));
@@ -67,7 +85,7 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
 
     const successHandler = () => {
         setWasSuccessful(true);
-        trackEvent({ category: 'Action', action: 'Donation', value: amount, nonInteraction: false });
+        trackEvent({ category: 'Action', action: 'Golden Buzzer', value: amount, nonInteraction: false });
     };
 
     const failureHandler = () => {
@@ -76,7 +94,7 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
 
     useEffect(() => {
         if (open) {
-            setAmount(donation.default.general);
+            setAmount(donation.default.golden_buzzer);
             setMessage('');
             setWasSuccessful(false);
             setFailed(false);
@@ -85,29 +103,47 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
 
     return (
         <Dialog open={isOpen} onOpenChange={closeDialog}>
-            <DialogContent className="lg:max-w-3xl">
-                <DialogTitle>Make a Donation</DialogTitle>
-                <DialogDescription>
-                    <b className="font-semibold">Donate to SilentMode</b> to support his efforts: whether you appreciate
-                    the work that went into building this project, the songs, or SilentMode himself.<br/>
-                    You can also donate directly to <Link className="underline font-semibold"
-                                                          href="https://www.kidscape.org.uk/support-us/donate"
-                                                          target="_blank">Kidscape</Link>: a
-                    London-based charity with a focus on anti-bullying and child safety.
-                </DialogDescription>
+            <DialogContent className="lg:max-w-3xl bg-amber-300 dark:bg-yellow-800" aria-describedby={undefined}>
+                <DialogTitle>Award a <span
+                    className="text-amber-700 dark:text-amber-300">Golden Buzzer</span>!</DialogTitle>
+
+                {song ? (
+                    <div>
+                        <div className="flex gap-2 items-center">
+                            <ActImage act={song.act} size="20"/>
+                            <div className="flex flex-col display-text leading-none">
+                                <span>{song.act.name}</span>
+                                <span className="text-sm flex gap-2 items-center">
+                                    <LanguageFlag languageCode={song?.language}/>
+                                    {song.title}
+                                </span>
+                            </div>
+                        </div>
+                        <p className="my-2">The Song will receive the same perks as a runner-up in this Stage.</p>
+                        <p className="text-xs">
+                            <b>IMPORTANT:</b> Golden Buzzers are for supporting your favourite Acts and Songs &ndash;
+                            they <b>do not count toward scoring</b>.
+                        </p>
+                    </div>) : ''}
 
                 {wasSuccessful ? (
                     <div
-                        className="h-1/3 p-10 flex flex-col text-center items-center justify-center text-green-600 font-semibold">
+                        className="h-1/3 p-10 text-amber-600 font-semibold gap-3">
                         <div className="mx-auto relative">
                             <ConfettiExplosion {...confettiSettings} />
                         </div>
-                        Thank you for your donation!
+                        <div className="max-w-2/3 flex flex-col text-center items-center justify-center">
+                            <p className="text-lg font-semibold">Thank you very much!</p>
+                            <p>You've just awarded a Golden Buzzer
+                                to <b>{song?.act.name}</b> in <b>{round?.full_title}</b>.
+                            </p>
+                            <p>Remember to cast your vote for your favourite Acts in this Round!</p>
+                        </div>
                     </div>
                 ) : (
                     <>
                         <div
-                            className="flex flex-col lg:flex-row gap-3 justify-between items-center py-2 px-5 bg-green-200 dark:bg-green-700 rounded-sm">
+                            className="flex flex-col lg:flex-row gap-3 justify-between items-center py-2 px-5 bg-amber-200 dark:bg-yellow-600 rounded-sm">
                             <Label htmlFor="donationAmount">I would like to donate</Label>
 
                             <div className="flex gap-1 items-center">
@@ -117,7 +153,7 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
                                 ))}
                                 <div className="ml-2 flex items-center">
                                     <Input
-                                        className="bg-white w-[5rem] border-green-500 text-green-800 font-semibold text-right text-lg"
+                                        className="bg-white w-[5rem] border-amber-500 dark:border-amber-800 text-amber-800 dark:text-amber-900 font-semibold text-right text-lg"
                                         id="donationAmount" type="number" value={amount} min="1"
                                         onChange={amountHandler}/>
                                     <span
@@ -146,7 +182,7 @@ export const DonateDialog: FC<DonateDialogProps> = () => {
 
                             <PaypalButton amount={amount} currency={donation.currency}
                                           additionalData={{ is_anonymous: isAnonymous, message }}
-                                          description="Real Figures Don't F.O.L.D: donation"
+                                          description="Real Figures Don't F.O.L.D: Golden Buzzer"
                                           onProcessing={processingHandler}
                                           onSuccess={successHandler}
                                           onFailure={failureHandler}/>
