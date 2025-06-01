bd5681fb096b408dc3d1560471808cb7c944073d
Drew Maughan <drew.maughan@toptal.com>
2025-06-01 15:56:50 +0100

    test: added a test suite for the Golden Buzzers admin page.

---
+++ tests/Feature/Controllers/Back/GoldenBuzzerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Controllers\Back;
+
+use App\Models\GoldenBuzzer;
+use App\Models\Stage;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Inertia\Testing\AssertableInertia as Assert;
+use PHPUnit\Framework\Attributes\Depends;
+use Tests\TestCase;
+
+class GoldenBuzzerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_as_guest()
+    {
+        $response = $this->get(route('admin.golden-buzzers'));
+
+        $response->assertRedirectToRoute('login');
+    }
+
+    public function test_as_user()
+    {
+        $response = $this->actingAs($this->user)->get(route('admin.golden-buzzers'));
+
+        $response->assertOk();
+        $response->assertInertia(fn(Assert $page) => $page->component('back/golden-buzzer'));
+    }
+
+    #[Depends('test_as_user')]
+    public function test_with_golden_buzzers()
+    {
+        Stage::factory()->withRounds()->create();
+        GoldenBuzzer::factory(21)->create();
+
+        $response = $this->actingAs($this->user)->get(route('admin.golden-buzzers'));
+
+        $response->assertOk();
+    }
+
+}
