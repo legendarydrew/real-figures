d1fa3a4aae660ef2230928f63c801c28329b4c66
Drew Maughan <drew.maughan@toptal.com>
2025-06-01 16:20:08 +0100

    test: adding a test suite for the Donations admin page.

--- tests/Feature/Controllers/Back/DonationsTest.php
+++ tests/Feature/Controllers/Back/DonationsTest.php
@@ -2,39 +2,39 @@
 
 namespace Controllers\Back;
 
-use App\Models\GoldenBuzzer;
+use App\Models\Donation;
 use App\Models\Stage;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Inertia\Testing\AssertableInertia as Assert;
 use PHPUnit\Framework\Attributes\Depends;
 use Tests\TestCase;
 
-class GoldenBuzzerTest extends TestCase
+class DonationsTest extends TestCase
 {
     use DatabaseMigrations;
 
     public function test_as_guest()
     {
-        $response = $this->get(route('admin.golden-buzzers'));
+        $response = $this->get(route('admin.donations'));
 
         $response->assertRedirectToRoute('login');
     }
 
     public function test_as_user()
     {
-        $response = $this->actingAs($this->user)->get(route('admin.golden-buzzers'));
+        $response = $this->actingAs($this->user)->get(route('admin.donations'));
 
         $response->assertOk();
-        $response->assertInertia(fn(Assert $page) => $page->component('back/golden-buzzers'));
+        $response->assertInertia(fn(Assert $page) => $page->component('back/donations'));
     }
 
     #[Depends('test_as_user')]
-    public function test_with_golden_buzzers()
+    public function test_with_donations()
     {
         Stage::factory()->withRounds()->create();
-        GoldenBuzzer::factory(21)->create();
+        Donation::factory(21)->create();
 
-        $response = $this->actingAs($this->user)->get(route('admin.golden-buzzers'));
+        $response = $this->actingAs($this->user)->get(route('admin.donations'));
 
         $response->assertOk();
     }
