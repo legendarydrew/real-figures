f36f1cc65a8fec82da3bae34925785358658be68
Drew Maughan <drew.maughan@toptal.com>
2025-06-04 15:05:35 +0100

    feat: position the song banner at the bottom of the page.

--- resources/js/components/front/song-player.tsx
+++ resources/js/components/front/song-player.tsx
@@ -26,15 +26,17 @@ export const SongPlayer: React.FC = () => {
     }, [isPlayerOpen]);
 
     return (isPlayerOpen && currentSong) ? (
-        <aside className="w-full max-w-[400px]">
-            <Button variant="ghost" type="button" title="Close the player" onClick={closeSongPlayer}>
-                <XIcon/>
-            </Button>
-            <div className="flex">
-                <SongBanner song={currentSong}/>
-                <Button variant="gold" type="button" title="Award a Golden Buzzer!">
-                    <StarIcon/>
-                </Button>
+        <aside className="z-10 fixed bottom-10 rounded-sm bg-gray-200 dark:bg-gray-700 border-1 shadow-lg w-full max-w-[400px]">
+            <div className="flex items-center">
+                <SongBanner className="flex-grow" song={currentSong}/>
+                <div className="toolbar gap-1">
+                    <Button variant="gold" type="button" title="Award a Golden Buzzer!">
+                        <StarIcon/>
+                    </Button>
+                    <Button variant="ghost" type="button" title="Close the player" onClick={closeSongPlayer}>
+                        <XIcon/>
+                    </Button>
+                </div>
             </div>
         </aside>
     ) : '';
--- resources/js/components/song-banner.tsx
+++ resources/js/components/song-banner.tsx
@@ -15,7 +15,7 @@ export const SongBanner: React.FC<SongBannerProps> = ({ song, className, ...prop
             <div className="bg-secondary/15 rounded-md leading-none">
                 <ActImage act={song.act}/>
             </div>
-            <div className="p-3 flex-grow text-left">
+            <div className="px-3 py-1 flex-grow text-left">
                 <div className="text-sm font-semibold truncate">{song.act.name}</div>
                 <div className="flex gap-2 text-xs items-center font-semibold truncate">
                     <LanguageFlag languageCode={song.language}/>
