6ccc7406b919107af5427bd9d6244b477df5a0e5
Drew Maughan <drew.maughan@toptal.com>
2025-06-05 13:55:04 +0100

    feat: display the current Stage's Golden Buzzer perks in the Golden Buzzer dialog.

---
+++ app/Transformers/CurrentStageTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Stage;
+use Illuminate\Support\Str;
+use League\Fractal\TransformerAbstract;
+
+class CurrentStageTransformer extends TransformerAbstract
+{
+
+    public function transform(Stage $stage): array
+    {
+        return [
+            'id'                  => (int)$stage->id,
+            'title'               => $stage->title,
+            'golden_buzzer_perks' => Str::markdown($stage->golden_buzzer_perks)
+        ];
+    }
+
+}
