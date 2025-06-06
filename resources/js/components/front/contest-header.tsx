06e41e833d04aa5de2419fa1490aef972c712be3
Drew Maughan <drew.maughan@toptal.com>
2025-06-06 17:20:39 +0100

    fix: updating the home page for mobile devices.

--- resources/js/components/front/contest-header.tsx
+++ resources/js/components/front/contest-header.tsx
@@ -2,7 +2,7 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 
 export default function ContestHeader({ children }) {
     return (
-        <div className="bg-indigo-200 dark:bg-indigo-800/60 md:py-10 md:px-5">
+        <div className="bg-indigo-200 dark:bg-indigo-800/60 p-5 md:py-10 md:px-5">
             <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-start gap-4">
 
                 <PlaceholderPattern className="stroke-neutral-900/20 dark:stroke-neutral-100/20"/>
