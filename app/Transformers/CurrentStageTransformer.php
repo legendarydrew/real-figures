aeefd516014599f47ea2c711e77a3f1a2bd412ed
Drew Maughan <drew.maughan@toptal.com>
2025-06-08 11:26:25 +0100

    fix: it was AppServiceProvider.php! It was prematurely fetching the current Stage (before migrations were performed).

---
+++ app/Transformers/CurrentStageTransformer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Stage;
+use Illuminate\Support\Str;
+use League\Fractal\TransformerAbstract;
+
+class CurrentStageTransformer extends TransformerAbstract
+{
+
+    public function transform(Stage $stage): array
+    {
+        return [
+            'id'                  => (int)$stage->id,
+            'title'               => $stage->title,
+            'golden_buzzer_perks' => $stage->golden_buzzer_perks ? Str::markdown($stage->golden_buzzer_perks) : '',
+            'status'              => [
+                'text' => $stage->status
+            ],
+        ];
+    }
+
+}
