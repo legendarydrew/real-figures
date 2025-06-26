76fd3f5685dd0dcfdf4a0a78d75e9e70fd60cbc0
Drew Maughan <drew.maughan@toptal.com>
2025-06-26 15:36:34 +0100

    feat: populate information on the generate page based on the selected post type.

--- app/Http/Controllers/Back/NewsGenerateController.php
+++ app/Http/Controllers/Back/NewsGenerateController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Back;
 
 use App\Enums\NewsPostType;
 use App\Http\Controllers\Controller;
+use App\Models\Act;
+use App\Models\Round;
+use App\Models\Stage;
 use Inertia\Inertia;
+use Inertia\Response;
 
 /**
  * NewsGenerateController
@@ -15,17 +19,39 @@ use Inertia\Inertia;
 class NewsGenerateController extends Controller
 {
 
-    public function index()
+    public function index(): Response
     {
-        // What kind of press releases do we want to generate:
-        // - CONTEST - a general post about the contest.
-        // - STAGE - specifically about a Stage and its Rounds.
-        // - ROUND - specifically about a Round within a Stage.
-        // - ACT - specifically about an Act.
-        // - CUSTOM - at the user's discretion.
-
+        /**
+         * What kind of press releases do we want to generate:
+         *
+         * - CONTEST - a general post about the contest.
+         * - STAGE - specifically about a Stage and its Rounds.
+         *  - provide a list of active, in progress and ended Stages.
+         * - ROUND - specifically about a Round within a Stage.
+         * - ACT - specifically about an Act.
+         * - CUSTOM - at the user's discretion.
+         *
+         * We want to ensure that we only fetch information we need.
+         */
         return Inertia::render('back/news-generate', [
-            'types' => NewsPostType::cases()
+            'types'  => NewsPostType::cases(),
+            'stages' => Inertia::optional(fn() => Stage::all()
+                                                       ->filter(fn(Stage $stage) => !$stage->isInactive())
+                                                       ->map(fn(Stage $stage) => [
+                                                           'id'     => $stage->id,
+                                                           'title'  => $stage->title,
+                                                           'status' => $stage->status
+                                                       ])),
+            'rounds' => Inertia::optional(fn() => Round::started()->whereHas('songs')->get()
+                                                       ->map(fn(Round $round) => [
+                                                           'id'    => $round->id,
+                                                           'title' => $round->full_title
+                                                       ])),
+            'acts'   => Inertia::optional(fn() => Act::whereHas('songs')->orderBy('name')->get()
+                                                     ->map(fn(Act $act) => [
+                                                         'id'   => $act->id,
+                                                         'name' => $act->name
+                                                     ]))
         ]);
     }
 }
--- resources/js/pages/back/news-generate.tsx
+++ resources/js/pages/back/news-generate.tsx
@@ -6,11 +6,21 @@ import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/u
 import { Button } from '@/components/ui/button';
 import { LoadingButton } from '@/components/mode/loading-button';
 import { titleCase } from '@/lib/utils';
+import { Badge } from '@/components/ui/badge';
+import { Checkbox } from '@/components/ui/checkbox';
 
-export default function NewsGeneratePage({ types }: Readonly<{ types }>) {
+interface NewsGeneratePageProps {
+    types: string[];
+    acts?: { id: number, name: string; }[];
+    rounds?: { id: number; title: string; }[];
+    stages?: { id: number; title: string; status: string; }[];
+}
+
+export default function NewsGeneratePage({ types, acts, rounds, stages }: Readonly<NewsGeneratePageProps>) {
 
     const { data, setData } = useForm({
         type: undefined,
+        references: [],
         title: '',
         content: ''
     });
@@ -22,9 +32,36 @@ export default function NewsGeneratePage({ types }: Readonly<{ types }>) {
     };
 
     const selectTypeHandler = (type: string): void => {
-        console.log('selectTypeHandler', type);
         setData('type', type);
+
+        let additionalInfo = [];
+        switch (type) {
+            case 'stage':
+                additionalInfo = ['stages'];
+                break;
+            case 'act':
+                additionalInfo = ['acts'];
+                break;
+            case 'round':
+                additionalInfo = ['rounds'];
+                break;
+            default:
+                additionalInfo = [];
+        }
+
+        router.reload({
+            only: additionalInfo,
+            showProgress: true,
+            onSuccess: () => {
+                setData('references', []);
+            }
+        });
     };
+
+    const selectSingleReferenceHandler = (value: number): void => {
+        console.log('select reference', value);
+        setData('references', [value]);
+    }
 
     return (
         <AppLayout>
@@ -48,6 +85,53 @@ export default function NewsGeneratePage({ types }: Readonly<{ types }>) {
                         </SelectContent>
                     </Select>
                 </div>
+
+                {/* A list of Stages (if available). */}
+                {(data.type === 'stage' && stages) && (
+                    <div>
+                        <Label className="sr-only" htmlFor="postReference">Select a Stage</Label>
+                        <Select id="postReference" onValueChange={selectSingleReferenceHandler}>
+                            <SelectTrigger>{data.reference_id ?? 'Select a Stage...'}</SelectTrigger>
+                            <SelectContent>
+                                {stages.map((stage) => (
+                                    <SelectItem key={stage.id} value={stage.id}>
+                                        {stage.title} <Badge>{stage.status}</Badge>
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                )}
+
+                {/* A list of Rounds (if available). */}
+                {(data.type === 'round' && rounds) && (
+                    <div>
+                        <Label className="sr-only" htmlFor="postReference">Select a Round</Label>
+                        <Select id="postReference" onValueChange={selectSingleReferenceHandler}>
+                            <SelectTrigger>{data.reference_id ?? 'Select a Round...'}</SelectTrigger>
+                            <SelectContent>
+                                {rounds.map((round) => (
+                                    <SelectItem key={round.id} value={round.id}>
+                                        {round.title}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                )}
+
+                {/* A list of Acts (if available). */}
+                {/* We would like to be able to select one or more Acts. */}
+                {(data.type === 'act' && acts) && (
+                    <fieldset>
+                        <legend className="font-normal text-sm mb-3">Select one or more Acts...</legend>
+                        {acts.map((act) => (
+                            <Label key={act.id} className="flex items-center gap-2">
+                                <Checkbox value={act.id}/> {act.name}
+                            </Label>
+                        ))}
+                    </fieldset>
+                )}
 
                 <div className="bg-white border-t-1 flex justify-between sticky bottom-0 py-3">
                     <Button variant="ghost" type="button" size="lg" onClick={cancelHandler}>Cancel</Button>
