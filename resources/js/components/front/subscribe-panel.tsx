5bd12b86fb2175b0d7071f21b41d8658ddaa0d48
Drew Maughan <drew.maughan@toptal.com>
2025-07-04 14:48:31 +0100

    fix: appearance of the about banner and subscribe panel.

--- resources/js/components/front/subscribe-panel.tsx
+++ resources/js/components/front/subscribe-panel.tsx
@@ -1,11 +1,11 @@
 import HeadingSmall from '@/components/heading-small';
-import {SubscribeForm} from '@/components/front/subscribe-form';
+import { SubscribeForm } from '@/components/front/subscribe-form';
 import { cn } from '@/lib/utils';
 
 export const SubscribePanel: React.FC = ({ className }) => {
 
     return (
-        <div className={cn("rounded-md p-3", className)}>
+        <div className={cn("rounded-md md:p-3", className)}>
             <HeadingSmall title="Subscribe for updates!"/>
             <p className="my-3 text-sm">Stay updated about the contest's progress, and be informed about when it's time
                 to cast your votes! Your details will not be used for anything else.</p>
