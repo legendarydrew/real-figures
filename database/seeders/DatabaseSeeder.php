30e5d53bd1c738287bd581e2151e728a8cba5a98
Drew Maughan <drew@pzlabs.co>
2026-02-17 11:57:55 +0000

    fix: adding seeding debug comments, reverting to $this->faker in UserFactory.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,23 +24,33 @@ class DatabaseSeeder extends Seeder
 
         $this->call(LanguagesSeeder::class);
 
-        if ( app()->environment() !== 'production' ) {
-            // Create test data.
+        // Test data for non-live environments.
+        if (!app()->isProduction())
+        {
+            $this->command->comment('Creating test user...');
             User::factory()->create([
                 'name'     => 'Test User',
                 'email'    => 'holla@silentmode.tv',
                 'password' => Hash::make('temp'),
             ]);
 
+            $this->command->comment('Creating dummy Acts...');
             Act::factory(4)->withPicture(50)->withProfile()->withMeta()->withSong()->create();
             Act::factory(4)->withPicture(50)->withProfile()->withSong()->create();
             Act::factory(4)->withPicture(50)->withProfile()->create();
             Act::factory(4)->withPicture(50)->withSong()->create();
             Act::factory(4)->withPicture(50)->create();
 
+            $this->command->comment('Creating dummy Stages...');
             Stage::factory()->withResults()->create();
+
+            $this->command->comment('Creating dummy Donations...');
             Donation::factory(fake()->numberBetween(1, 20))->create();
+
+            $this->command->comment('Creating dummy Subscribers...');
             Subscriber::factory(fake()->numberBetween(1, 20))->create();
+
+            $this->command->comment('Creating dummy contact messages...');
             ContactMessage::factory(fake()->numberBetween(1, 40))->create();
         }
     }
