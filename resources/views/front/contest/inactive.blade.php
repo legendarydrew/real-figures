ecd85b31381690e0e888a3fbf08013a80b337e0e
Drew Maughan <drew@pzlabs.co>
2026-02-19 11:10:40 +0000

    feat: adding a background image to Contest pages.

--- resources/views/front/contest/inactive.blade.php
+++ resources/views/front/contest/inactive.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
 
-    <div class="contest">
+    <div class="contest" style="background-image: url({{ asset('img/bg-stage.jpg') }}">
 
         <header class="contest-header">
+            <h1>The Song Contest is On The Way.</h1>
             <p><b>Be the first to hear when voting opens, songs drop and surprises land.</b> Subscribe below to get
                 updates straight to your inbox.
         </header>
