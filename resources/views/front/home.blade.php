5bd91044141ee618d38295c2fbfa41f6e5a8f1de
Drew Maughan <drew@pzlabs.co>
2026-02-20 13:58:27 +0000

    feat: Display different hero content if the Contest is over.

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,17 +1,40 @@
 @extends('front.layout')
 
+@section('page-description', '32 Acts, One Anthem. Discover the CATAWOL Records Song Contest, where your vote decides the winner. Follow the journey and support music that makes a difference.')
+
 @section('content')
 
-    <div class="home-hero">
+    <div class="home-hero{{\App\Facades\ContestFacade::isOver() ? ' contest-over' : ''}}">
         <div class="site-container">
             <div class="home-hero-mic" style="background-image: url({{ asset('img/microphone.png') }}"></div>
-            <div class="class home-hero-text">
-                <h1>32 Acts. One Anthem.</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cumque delectus dolores
-                    exercitationem labore maiores mollitia nam numquam obcaecati officia optio perferendis perspiciatis
-                    quam, quas quos reiciendis repudiandae sequi tempora?</p>
+            <div class="home-hero-content">
+                <div class="home-hero-text">
+                    <h1>32 Acts. One Anthem.</h1>
+                    @if(\App\Facades\ContestFacade::isOver())
+                        <p>We've raised awareness about <b>bullying</b> through music &mdash;
+                            and <b>you helped pick the winner!</b></p>
+                    @else
+                        <p>We're raising awareness about <b>bullying</b> through music &mdash;
+                            and <b>you</b> help pick the winner!</p>
+                    @endif
+                </div>
+                <div class="home-hero-subscribe">
+                    @if(\App\Facades\ContestFacade::isOver())
+                        <p>The Contest is now over. Visit the <a href="{{ route('contest') }}">Contest page</a> to find
+                            out which Acts came out on top.</p>
+                    @else
+                        <h2>Subscribe for updates!</h2>
+                        <subscribe-form></subscribe-form>
+                        <p>
+                            Stay updated about the contest's progress, and be informed about when
+                            it's time to cast your votes!<br>
+                            <em>Your details will not be used for anything else.</em>
+                        </p>
+                    @endif
+                </div>
             </div>
         </div>
+    </div>
     </div>
 
     <div class="site-container">
