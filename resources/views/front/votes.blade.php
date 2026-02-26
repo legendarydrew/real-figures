94e9846b9bc6eb53cf866ddb8588cfd348b5b3b6
Drew Maughan <drew@pzlabs.co>
2026-02-26 12:31:43 +0000

    feat: adding some styling.

--- resources/views/front/votes.blade.php
+++ resources/views/front/votes.blade.php
@@ -12,41 +12,54 @@
     <div class="site-container">
 
         @foreach ($stages as $stage)
-            <div
-                class="vote-breakdown w-full flex gap-2 items-center justify-center cursor-pointer bg-zinc-700 hover:bg-zinc-600 text-center p-2">
+            <div class="votes-stage">
                 <h2 class="display-text text-lg">{{$stage['title']}}</h2>
 
                 @foreach($stage['breakdowns'] as $breakdown)
                     {{-- each Round in the Stage. --}}
-                    <div class="flex bg-zinc-600 py-2 text-sm font-semibold items-end leading-none sticky-top">
-                        <div class="flex-grow flex gap-3 items-end">
-                            <h3 class="display-text pl-3 text-base">{{$breakdown['title']}}</h3>
-                            {{--                            {wasManualOutcome() && <Badge variant="secondary" title="Votes cast by an independent panel.">Judged</Badge>}--}}
-                        </div>
-                        <div class="w-[6em] px-3 text-right">
-                            <span class="text-xs">Score</span>
-                        </div>
-                        <div class="w-[6em] px-3 text-right">
-                            <span class="text-xs">1st<br/>choice</span>
-                        </div>
-                        <div class="w-[6em] px-3 text-right">
-                            <span class="text-xs">2nd<br/>choice</span>
-                        </div>
-                        <div class="w-[6em] px-3 text-right">
-                            <span class="text-xs">3rd<br/>choice</span>
-                        </div>
+                    {{--
+                    What we want to see:
+                    - whether a Song received a Golden Buzzer in that round;
+                    - which Song(s) won (winner or runner-up).
+                                   --}}
+                    <div class="votes-round">
+                        @if (count($stage['breakdowns']) > 1)
+                            <h3>{{$breakdown['title']}}</h3>
+                        @endif
+                        @if($breakdown['manual_vote'])
+                            <p class="text-sm">Winners were determined by an independent panel.</p>
+                        @endif
+
+                        <table class="votes-breakdown">
+                            <thead>
+                            <tr>
+                                <th scope="col">Golden<br>Buzzer</th>
+                                <th scope="col"></th>
+                                <th scope="col">Score</th>
+                                <th scope="col">1st choice</th>
+                                <th scope="col">2nd choice</th>
+                                <th scope="col">3rd choice</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($breakdown['songs'] as $song)
+                                <tr>
+                                    <td></td>
+                                    <th scope="row">
+                                        <div class="flex gap-2 items-center">
+                                            @include('front.act-image', ['act' => $song['song']['act'], 'size' => 8])
+                                            {{ $song['song']['act']['name'] }}
+                                        </div>
+                                    </th>
+                                    <td class="text-right font-semibold">{{$song['score']}}</td>
+                                    <td class="text-sm text-right">{{$song['first_choice_votes']}}</td>
+                                    <td class="text-sm text-right">{{$song['second_choice_votes']}}</td>
+                                    <td class="text-sm text-right">{{$song['third_choice_votes']}}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                    <ul>
-                        @foreach($breakdown['songs'] as $song)
-                            <li class="flex items-center hover:bg-zinc-200/20 p-1 leading-tight rounded-sm">
-                                {{--                                <SongBanner class="flex-grow" song={song.song}/>--}}
-                                <div class="w-[6em] p-3 text-right font-semibold">{{$song['score']}}</div>
-                                <div class="w-[6em] p-3 text-sm text-right">{{$song['first_choice_votes']}}</div>
-                                <div class="w-[6em] p-3 text-sm text-right">{{$song['second_choice_votes']}}</div>
-                                <div class="w-[6em] p-3 text-sm text-right">{{$song['third_choice_votes']}}</div>
-                            </li>
-                        @endforeach
-                    </ul>
                 @endforeach
             </div>
         @endforeach
