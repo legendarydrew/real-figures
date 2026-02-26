677b5b8b492d44efa5466685afa1df280f2e4cc3
Drew Maughan <drew@pzlabs.co>
2026-02-26 17:58:03 +0000

    feat: consolidation of styles used on the Contact page.

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -8,22 +8,22 @@
 @endsection
 
 @section('content')
-    <div class="site-container my-8">
+    <div class="site-container">
         <div class="page-banner">
-            <img src="{{ asset('img/banners/contact-2.jpg') }}" alt="Minifigures reading and handling fanmail.">
+            <img src="{{ asset('img/banners/contact-2.jpg') }}" alt="Minifigures reading and handling fan mail.">
         </div>
 
         <h1 class="page-heading">Contact Us</h1>
 
-        <div class="grid grid-cols-1 lg:grid-cols-5 lg:gap-8">
-            <div class="content lg:col-span-2">
+        <div class="contact-layout">
+            <div class="content">
                 <p>Have a question about the Contest? Want to know more about the voting process,
                     submissions, or the Golden Buzzer? We’d love to hear from you!</p>
                 <p>Whether you're an artist, a voter, a supporter, or just curious, we’re all ears.</p>
 
                 @include('front.advert')
             </div>
-            <div class="lg:col-span-3">
+            <div class="contact-layout-form">
                 <contact-form></contact-form>
             </div>
         </div>
