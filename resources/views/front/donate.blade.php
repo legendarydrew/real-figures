5a7c6f9d9bf9fee1ee3079aeb16dc1124f6e30b3
Drew Maughan <drew@pzlabs.co>
2026-02-26 17:41:18 +0000

    feat: consolidation of styles used on the Donate page.

--- resources/views/front/donate.blade.php
+++ resources/views/front/donate.blade.php
@@ -4,8 +4,7 @@
 @section('page-description', "Support the CATAWOL Records Song Contest and help us amplify creativity and raise awareness of bullying. Learn how to donate and make a difference.")
 
 @section('content')
-
-    <div class="site-container my-8">
+    <div class="site-container">
 
         <div class="page-banner">
             <img src="{{ asset('img/banners/donate-2.jpg') }}"
@@ -15,45 +14,47 @@
         <h1 class="page-heading">Your Donations</h1>
 
         <!-- Donation buttons. -->
-        <div class="my-8 flex flex-col md:flex-row justify-center items-center gap-4 md:gap-16">
+        <div class="donate-buttons">
 
             {{-- https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog#modal_dialogs_using_invoker_commands  --}}
-            <button type="button" class="button primary w-4/5"
+            <button type="button" class="button primary"
                     command="show-modal" commandfor="donate-dialog" aria-controls="donate-dialog">Donate to us (PayPal)
             </button>
 
-            <a class="button secondary w-4/5" href="https://www.justgiving.com/kidscape/donate" rel="external"
+            <a class="button secondary" href="https://www.justgiving.com/kidscape/donate" rel="external"
                target="_blank">Donate to Kidscape</a>
         </div>
 
-        <div class="content mb-8">
-            <p class="text-lg">
+        <div class="content">
+            <p class="content-intro">
                 If you're enjoying the Contest, please consider <b>making a donation:</b> either to us,
-                or to <a class="font-semibold hover:underline" href="https://kidscape.org.uk" rel="external"
-                         target="_blank">Kidscape</a>, our designated charity.
+                or to <a href="https://kidscape.org.uk" rel="external" target="_blank">Kidscape</a>, our chosen
+                charity.
             </p>
             <p>Donations made to us will go toward the costs associated with building and maintaining the site,
                 supporting our Acts and their music production, and aiding the MODE Family in their time of need.
             </p>
-            <p><a class="font-semibold hover:underline" href="https://kidscape.org.uk" rel="external" target="_blank">Kidscape</a>
+            <p><a href="https://kidscape.org.uk" rel="external" target="_blank">Kidscape</a>
                 provides telephone advice and works directly with parents, children and carers experiencing bullying,
                 across the United Kingdom.</p>
-            <p>You can also <a class="font-semibold hover:underline" href="{{route('contact')}}">contact
-                    us</a> directly with other suggestions for supporting the contest.</p>
+            <p>You can also <a href="{{route('contact')}}">contact us</a> directly with other suggestions for supporting
+                the contest.</p>
         </div>
 
         <!-- Donations -->
         @if (isset($donations) && count($donations))
+            <hr>
             <h2 class="page-subheading">Generous Donations</h2>
             <p>
                 <b>A huge thank you</b> for these generous donations:
             </p>
             <ul class="donations-list">
                 @foreach($donations as $donation)
-                    <li class="select-none">
-                <span
-                    class="flex-grow truncate {{ $donation['is_anonymous'] ? "italic" : "font-semibold"}}">{{ $donation['name']}}</span>
-                        <span class="text-xs text-right">{{ $donation['created_at'] }}</span>
+                    <li>
+                        <span class="donor-name {{ $donation['is_anonymous'] ? "anonymous" : "named"}}">
+                            {{ $donation['name']}}
+                        </span>
+                        <span class="donor-date">{{ $donation['created_at'] }}</span>
                     </li>
                 @endforeach
             </ul>
@@ -65,11 +66,11 @@
             <p>These donations were in support of specific Acts:</p>
             <ul class="golden-buzzers-list">
                 @foreach($buzzers as $donation)
-                    <li class="select-none">
-                <span class="{{ $donation['is_anonymous'] ? "anonymous" : "named" }}">
+                    <li>
+                <span class="donor-name {{ $donation['is_anonymous'] ? "anonymous" : "named" }}">
                     {{ $donation['name'] }}
                 </span>
-                        <span class="date">{{$donation['created_at']}}</span>
+                        <span class="donor-date">{{$donation['created_at']}}</span>
                     </li>
                 @endforeach
             </ul>
