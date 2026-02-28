64860ff4eefb400dcb4c38b5cec0d086ae407382
Drew Maughan <drew@pzlabs.co>
2026-02-28 15:36:18 +0000

    feat: adding a subtitle column to the Acts table.

--- database/migrations/2025_03_20_153648_create_acts_table.php
+++ database/migrations/2025_03_20_153648_create_acts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('acts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('subtitle')->nullable();
             $table->string('slug')->unique();
             $table->timestamps();
         });
