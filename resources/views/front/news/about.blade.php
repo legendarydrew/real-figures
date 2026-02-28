39f746b76db789af37691ed12bfaa22d6ef4514b
Drew Maughan <drew@pzlabs.co>
2026-02-28 09:12:49 +0000

    feat: adding an image to the footer of the News pages.

--- resources/views/front/news/about.blade.php
+++ resources/views/front/news/about.blade.php
@@ -1,6 +1,9 @@
 <aside class="news-aside" aria-labelledby="news-about">
-    <h2 id="news-about" class="page-subheading">About CATAWOL Records</h2>
-    <p>CATAWOL Records is a music label championing bold voices and meaningful messages. We
-        support artists who use creativity to challenge norms, raise awareness, and connect
-        through sound.</p>
+    <img src="{{ asset('img/banners/news-about-10.jpg') }}" alt="A representative engaging with another person,">
+    <div>
+        <h2 id="news-about" class="page-subheading">About CATAWOL Records</h2>
+        <p>CATAWOL Records is a music label championing bold voices and meaningful messages. We
+            support artists who use creativity to challenge norms, raise awareness, and connect
+            through sound.</p>
+    </div>
 </aside>
