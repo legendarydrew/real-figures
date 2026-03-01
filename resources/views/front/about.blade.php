b0790276f908c2c51fb501b2e8841ad73c87b8b9
Drew Maughan <drew@pzlabs.co>
2026-03-01 19:32:05 +0000

    feat: styling of the About page points.

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -14,37 +14,37 @@
 
         <h1 class="page-heading">Real Figures Don't F.O.L.D &mdash; About the Project</h1>
 
-        <div class="about-intro content">
+        <div class="about-intro">
             <p class="content-intro text-center">
                 <b>Real Figures Don't F.O.L.D combines SilentMode's interest in LEGO</b> with music,<br>
                 "artificial intelligence", web development and advocacy.
             </p>
 
-            <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-4 text-sm my-8 text-center lg:text-left">
-                <div class="p-2 lg:p-4">
+            <ul class="about-points">
+                <li class="content">
                     <strong>Revisiting one of SilentMode's earliest Creations.</strong><br/>
                     CATAWOL Records began life as a modular building, designed and built
                     near the beginning of SilentMode's time in the LEGO hobby.
-                </div>
-                <div class="p-2 lg:p-4">
+                </li>
+                <li class="content">
                     <strong>Embarking on an ambitious LEGO project.</strong><br/>
                     Expanding on his existing skills as a Maker, Artist and LEGO
                     Enthusiast, this is SilentMode's first project to fully incorporate music and
                     AI/computer-generated content.
-                </div>
-                <div class="p-2 lg:p-4">
+                </li>
+                <li class="content">
                     <strong>Creating the first ever anti-bullying campaign (that we know of) within the LEGO space.</strong><br/>
                     An opportunity to highlight an important issue,
                     affecting <b>both children and adults</b>, that has probably never been
                     addressed before in the context of LEGO.
-                </div>
-                <div class="p-2 lg:p-4">
+                </li>
+                <li class="content">
                     <strong>A live demonstration of coding ability.</strong><br/>
                     Designed and built by SilentMode himself, the site uses
                     Laravel with Inertia for the back end, and React with Tailwind for the front end.
-                </div>
+                </li>
 
-            </div>
+            </ul>
         </div>
 
         @include('front.advert')
