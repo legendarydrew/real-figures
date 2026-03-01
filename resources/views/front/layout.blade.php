d6fdc541a66ea74dd868930d3454cf4878e434dd
Drew Maughan <drew@pzlabs.co>
2026-03-01 22:24:57 +0000

    feat: updating page titles.

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@hasSection('page-title')@yield('page-title') &mdash; @endif{{ config('app.name') }}</title>
     <title>@yield('page-title', config('app.name'))</title>
     <meta name="description" content="@yield('page-description')"/>
     <meta name="paypal-client" content="{{ config('services.paypal.client_id')}}"/>
