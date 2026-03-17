9daee5dd9b6a18b2f762798c676f3127cfa126a1
Drew Maughan <drew@pzlabs.co>
2026-03-17 19:07:28 +0000

    feat: applying chart styles to the page views chart on the dashboard.
    Introduced convenience components for consistent chart styles.

--- resources/js/components/admin/dashboard-analytics-views.tsx
+++ resources/js/components/admin/dashboard-analytics-views.tsx
@@ -1,9 +1,10 @@
-import { CartesianGrid, Line, LineChart, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip } from 'recharts';
 import { Nothing } from '@/components/mode/nothing';
 import { usePage } from '@inertiajs/react';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { cssVar } from '@/lib/utils';
 import { useEffect, useRef } from 'react';
+import { ChartDateXAxis, ChartRoundReferences, ChartYAxis, formatDate } from '@/components/chart-elements';
 
 type DashboardAnalyticsViewsData = {
     date: string;
@@ -39,40 +40,30 @@ const ChartReference = ({ round, props }) => {
 
     return (
         <g transform={`translate(${props.viewBox.x - props.offset},${props.viewBox.height - props.offset})rotate(${props.angle})`}>
-            <rect fill="var(--secondary)" opacity={0.75} x={-props.offset} y={-props.offset * 2} width={boxWidth.current} height={boxHeight.current}></rect>
+            <rect fill="var(--secondary)" opacity={0.75} x={-props.offset} y={-props.offset * 2}
+                  width={boxWidth.current} height={boxHeight.current}></rect>
             <text ref={text} fontSize={props.fontSize} fontWeight="bold" fill="#FFF">{round.name}</text>
         </g>);
-} ;
+};
 
 export const DashboardAnalyticsViews: React.FC<DashboardAnalyticsViewsProps> = ({ data, className }) => {
 
-    const { locale, rounds } = usePage().props;
-
-    const formatDate = (timestamp: string): string => {
-        return new Date(timestamp).toLocaleDateString(locale as string);
-    };
-
-    const formatReferenceLine = (round) => ({
-        // value: round.name,
-        content: (props) => <ChartReference round={round} props={props} />,
-        fill: 'var(--secondary)',
-        position: 'insideBottomRight',
-        angle: -90,
-        fontSize: 10,
-        fontWeight: 'bold',
-        textAnchor: 'start'
-    });
+    const { locale } = usePage().props;
 
     const tooltipContent = ({ active, payload, label }) => {
         if (active && payload?.length) {
             return (
-                <div className="bg-white flex flex-col gap-0 shadow-md leading-tight rounded-sm p-3">
-                    <span className="display-text">{formatDate(label)}</span>
-                    <span className="text-sm">
-                        {payload[0].value ? payload[0].value.toLocaleString() : 'No'} {payload[0].value === 1 ? 'page view' : 'page views'}
+                <div className="bg-white flex flex-col gap-0 shadow-md leading-tight rounded-sm p-2">
+                    <span className="display-text text-sm">{formatDate(locale as string, label)}</span>
+                    <span className="flex items-center gap-1 text-xs">
+                        <span className="size-3 inline-block bg-(--chart-4-3)"></span>
+                        {payload[0].value ? payload[0].value.toLocaleString() : 'No'}&nbsp;
+                        {payload[0].value === 1 ? 'page view' : 'page views'}
                     </span>
-                    <span className="text-sm">
-                        {payload[1].value ? payload[1].value.toLocaleString() : 'No'} {payload[0].value === 1 ? 'Visitor' : 'Visitors'}
+                    <span className="flex items-center gap-1 text-xs">
+                        <span className="size-3 inline-block bg-(--chart-2-3)"></span>
+                        {payload[1].value ? payload[1].value.toLocaleString() : 'No'}&nbsp;
+                        {payload[1].value === 1 ? 'Visitor' : 'Visitors'}
                     </span>
                 </div>
             );
@@ -86,27 +77,22 @@ export const DashboardAnalyticsViews: React.FC<DashboardAnalyticsViewsProps> = (
             <CardTitle className="display-text font-normal">
                 Page views <small>within the last week</small>
             </CardTitle>
-            <CardContent className="px-0">
+            <CardContent>
                 {data.length ? (
-                    <ResponsiveContainer className="w-full" aspect={4}>
-                        <LineChart data={data} margin={2}>
+                    <ResponsiveContainer width="100%" aspect={3} maxHeight={300}>
+                        <LineChart data={data} height={300}>
                             <CartesianGrid strokeDasharray="3 3"/>
-                            <XAxis dataKey="date"
-                                   tickFormatter={formatDate}
-                                   className="display-text font-normal text-xs"/>
-                            <YAxis className="display-text font-normal text-xs"/>
-                            <Tooltip content={tooltipContent} isAnimationActive={false}/>
-                            <Line dataKey="views" label="Page views" dot={false} strokeWidth={2}
-                                  stroke={cssVar('--primary')}/>
-                            <Line dataKey="visitors" label="Visitors" dot={false} strokeWidth={2}
-                                  stroke="var(--secondary')"/>
+                            <ChartDateXAxis/>
+                            <ChartYAxis dataKey="views" yAxisId="pageViews" label="Views"/>
+                            <ChartYAxis dataKey="visitors" yAxisId="pageVisitors" label="Visitors" orientation="right"/>
 
-                            {rounds.map((round) => (
-                                <ReferenceLine key={round.date} x={round.date}
-                                               stroke="var(--secondary)"
-                                               strokeWidth={2}
-                                               label={formatReferenceLine(round)}/>
-                            ))}
+                            <Tooltip content={tooltipContent} isAnimationActive={false}/>
+                            <Line dataKey="views" yAxisId="pageViews" label="Page views" dot={false} strokeWidth={2}
+                                  stroke={cssVar('--chart-4-3')}/>
+                            <Line dataKey="visitors" yAxisId="pageVisitors" label="Visitors" dot={false} strokeWidth={2}
+                                  stroke={cssVar('--chart-2-3')}/>
+
+                            <ChartRoundReferences/>
                         </LineChart>
                     </ResponsiveContainer>
                 ) : (
