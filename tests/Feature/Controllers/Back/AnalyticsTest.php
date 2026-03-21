9639c9e89abab54ddcb16d49c92d93a0f76d3b1e
Drew Maughan <drew@pzlabs.co>
2026-03-21 18:41:52 +0000

    test: added a test suite for the analytics back office page.

---
+++ tests/Feature/Controllers/Back/AnalyticsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers\Back;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\TestCase;
+
+class AnalyticsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_as_guest()
+    {
+        $response = $this->get(route('admin.analytics'));
+
+        $response->assertRedirectToRoute('login');
+    }
+
+    public function test_as_user()
+    {
+        $response = $this->actingAs($this->user)->get(route('admin.analytics'));
+
+        $response->assertOk();
+        $response->assertInertia(fn(Assert $page) => $page->component('back/analytics-page'));
+    }
+
+}
