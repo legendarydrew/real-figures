993aef24def9af335ab54a320837cc9e3f199759
Drew Maughan <drew@pzlabs.co>
2026-03-22 09:02:54 +0000

    test: added DestroyTest.php for the News endpoint.

---
+++ tests/Feature/Controllers/Back/News/DestroyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Controllers\Back\News;
+
+use App\Models\NewsPost;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class DestroyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private NewsPost $post;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->post = NewsPost::factory()->createOne();
+    }
+
+    public function test_as_guest()
+    {
+        $response = $this->delete(route('news.destroy', ['id' => $this->post->id]));
+
+        $response->assertRedirectToRoute('login');
+    }
+
+    public function test_as_user()
+    {
+        $response = $this->actingAs($this->user)->delete(route('news.destroy', ['id' => $this->post->id]));
+
+        $response->assertRedirectToRoute('admin.news');
+    }
+
+    public function test_invalid_post()
+    {
+        $response = $this->actingAs($this->user)->delete(route('news.destroy', ['id' => 404]));
+
+        $response->assertNotFound();
+    }
+
+}
