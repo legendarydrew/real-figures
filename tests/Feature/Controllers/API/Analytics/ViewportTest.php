b333f3bbf44acb89736275aa9a8dc05cfdbc805f
Drew Maughan <drew@pzlabs.co>
2026-03-22 16:01:20 +0000

    test: added ViewportTest.php.

---
+++ tests/Feature/Controllers/API/Analytics/ViewportTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Controllers\API\Analytics;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ViewportTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected const string ENDPOINT  = 'api/analytics/viewports';
+    protected const int    DAY_COUNT = 7;
+
+    public function test_as_guest()
+    {
+        $response = $this->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+        $response->assertUnauthorized();
+    }
+
+    public function test_no_data()
+    {
+        \Analytics::fake(collect());
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(0, 'keys');
+        $response->assertJsonCount(0, 'data');
+        $response->assertJsonCount(0, 'table');
+        $response->assertJsonStructure([
+            'keys',
+            'data'  => [
+                '*' => [
+                    'date'
+                ]
+            ],
+            'table' => [
+                '*' => [
+                    'viewport',
+                    'views'
+                ]
+            ]
+        ]);
+    }
+
+    public function test_with_data()
+    {
+        \Analytics::fake(collect([
+            [
+                'date'                         => now()->subDay(),
+                'customEvent:visitor_viewport' => '1280x900',
+                'screenPageViews'              => fake()->numberBetween(1, 200)
+            ],
+            [
+                'date'                         => now()->subDays(2),
+                'customEvent:visitor_viewport' => '1280x900',
+                'screenPageViews'              => fake()->numberBetween(1, 200)
+            ],
+            [
+                'date'                         => now()->subDay(),
+                'customEvent:visitor_viewport' => '320x640',
+                'screenPageViews'              => fake()->numberBetween(1, 200)
+            ]
+        ]));
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(2, 'keys');  // two sets of dimensions.
+        $response->assertJsonCount(2, 'data');  // two different dates.
+        $response->assertJsonCount(2, 'table'); // two sets of dimensions.
+        $response->assertJsonStructure([
+            'keys',
+            'data'  => [
+                '*' => [
+                    'date'
+                ]
+            ],
+            'table' => [
+                '*' => [
+                    'viewport',
+                    'views'
+                ]
+            ]
+        ]);
+    }
+}
