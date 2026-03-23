36205bfd90686a4ab2c0b4cbb15de3f9bb103aaa
Drew Maughan <drew@pzlabs.co>
2026-03-23 19:36:14 +0000

    test: added OperatingSystemsTest.php.

---
+++ tests/Feature/Controllers/API/Analytics/OperatingSystemsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Controllers\API\Analytics;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class OperatingSystemsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected const string ENDPOINT  = 'api/analytics/os';
+    protected const int    DAY_COUNT = 7;
+
+    public function test_as_guest()
+    {
+        $response = $this->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+        $response->assertUnauthorized();
+    }
+
+    public function test_no_data()
+    {
+        \Analytics::fake(collect());
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(0);
+        $response->assertJsonStructure([
+            '*' => [
+                'operatingSystem',
+                'screenPageViews'
+            ]
+        ]);
+    }
+
+    public function test_with_data()
+    {
+        \Analytics::fake(collect([
+            [
+                'operatingSystem' => fake()->unique()->word,
+                'screenPageViews' => fake()->numberBetween(1, 200)
+            ],
+            [
+                'operatingSystem' => fake()->unique()->word,
+                'screenPageViews' => fake()->numberBetween(1, 200)
+            ]
+        ]));
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(2);
+        $response->assertJsonStructure([
+            '*' => [
+                'operatingSystem',
+                'screenPageViews'
+            ]
+        ]);
+    }
+}
