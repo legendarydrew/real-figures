c2b038566c8fafa70b306fc4b2e9bbd8e6131806
Drew Maughan <drew@pzlabs.co>
2026-03-24 00:45:32 +0000

    test: added VotesTest.php.

---
+++ tests/Feature/Controllers/API/Analytics/VotesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Controllers\API\Analytics;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class VotesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected const string ENDPOINT  = 'api/analytics/votes';
+    protected const int    DAY_COUNT = 7;
+
+    public function test_as_guest()
+    {
+        $response = $this->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+        $response->assertUnauthorized();
+    }
+
+    public function test_no_data()
+    {
+        \Analytics::fake(collect());
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(self::DAY_COUNT * 24 + 1);
+        $response->assertJsonStructure([
+            '*' => [
+                'time',
+                'count'
+            ]
+        ]);
+    }
+
+    public function test_with_data()
+    {
+        \Analytics::fake(collect([
+            [
+                'dateHour'    => now()->subDays(2)->format('YmdH'),
+                'eventCount' => fake()->numberBetween(1, 200)
+            ],
+            [
+                'dateHour'    => now()->subDays(3)->format('YmdH'),
+                'eventCount' => fake()->numberBetween(1, 200)
+            ]
+        ]));
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(self::DAY_COUNT * 24 + 1);
+        $response->assertJsonStructure([
+            '*' => [
+                'time',
+                'count'
+            ]
+        ]);
+    }
+}
