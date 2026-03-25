4eb9efbb6ee846a0dd0fbd87a4433fd974fe27b8
Drew Maughan <drew@pzlabs.co>
2026-03-25 12:21:47 +0000

    fix: renaming an Act's image file based on its slug.

---
+++ tests/Feature/ActImage/RenameTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ActImage;
+
+use App\Facades\ActImageFacade;
+use App\Models\Act;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Smknstd\FakerPicsumImages\FakerPicsumImagesProvider;
+use Tests\TestCase;
+
+class RenameTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private Act $act;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        fake()->addProvider(new FakerPicsumImagesProvider(fake()));
+
+        $this->act = Act::factory()->createOne();
+        $image     = fake()->image();
+        ActImageFacade::create($this->act, $image);
+
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileExists($path);
+
+        Act::bootHasEvents();
+    }
+
+    public function test_renames_existing_image_from_name_change()
+    {
+        $this->act->update([
+            'name' => fake()->name
+        ]);
+        ActImageFacade::rename($this->act);
+        $this->act->refresh();
+
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileExists($path);
+        @unlink($path);
+    }
+
+    public function test_renames_existing_image_from_subtitle_change()
+    {
+        $this->act->update([
+            'subtitle' => fake()->sentence
+        ]);
+        ActImageFacade::rename($this->act);
+
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileExists($path);
+        @unlink($path);
+    }
+
+    public function test_preserve_existing_image_with_same_slug()
+    {
+        ActImageFacade::rename($this->act);
+
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileExists($path);
+        @unlink($path);
+    }
+
+    public function test_missing_image()
+    {
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileExists($path);
+        @unlink($path);
+
+//        $this->expectException(FileNotFoundException::class);
+        ActImageFacade::rename($this->act);
+
+        $path = ActImageFacade::path($this->act);
+        $this->assertFileDoesNotExist($path);
+    }
+
+}
