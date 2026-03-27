e6842cd9d676f2670638cbc535231b3ae0cde8a0
Drew Maughan <drew@pzlabs.co>
2026-03-27 11:58:33 +0000

    fix: corrected the new Fillable attribute for models.

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable('email', 'confirmed')]
+#[Fillable(['email', 'confirmed'])]
 class Subscriber extends Model
 {
     /** @use HasFactory<\Database\Factories\SubscriberFactory> */
