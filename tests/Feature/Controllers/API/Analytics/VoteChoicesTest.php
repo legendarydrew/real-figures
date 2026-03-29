a8f1fe35a28d98c362f28d8435d75abf0cd2360c
Drew Maughan <drew@pzlabs.co>
2026-03-29 10:56:24 +0100

    test: added VoteChoicesTest.php.

---
+++ tests/Feature/Controllers/API/Analytics/VoteChoicesTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Controllers\API\Analytics;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+final class VoteChoicesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected const string ENDPOINT = 'api/analytics/votes/choices';
+
+    protected const int    DAY_COUNT = 7;
+
+    public function test_as_guest(): void
+    {
+        $response = $this->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+        $response->assertUnauthorized();
+    }
+
+    public function test_no_data(): void
+    {
+        \Analytics::fake(collect());
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(0, 'keys');
+        $response->assertJsonCount(self::DAY_COUNT + 1, 'data');
+        $response->assertJsonCount(0, 'table');
+        $response->assertJsonStructure([
+            'keys',
+            'data' => [
+                '*' => [
+                    'date',
+                    'total',
+                ],
+            ],
+            'table' => [
+                '*' => [
+                    'choices',
+                    'count',
+                ],
+            ],
+        ]);
+    }
+
+    public function test_with_data(): void
+    {
+        \Analytics::fake(collect([
+            [
+                'date' => now()->subDay(),
+                'customEvent:choices' => 3,
+                'eventCount' => fake()->numberBetween(1, 200),
+            ],
+            [
+                'date' => now()->subDay(),
+                'customEvent:choices' => 1,
+                'eventCount' => fake()->numberBetween(1, 200),
+            ],
+            [
+                'date' => now()->subDays(2),
+                'customEvent:choices' => 2,
+                'eventCount' => fake()->numberBetween(1, 200),
+            ],
+            [
+                'date' => now()->subDay(),
+                'customEvent:choices' => 3,
+                'eventCount' => fake()->numberBetween(1, 200),
+            ],
+        ]));
+
+        $response = $this->actingAs($this->user)->getJson(self::ENDPOINT, ['days' => self::DAY_COUNT]);
+
+        $response->assertOk();
+        $response->assertJsonCount(3, 'keys');
+        $response->assertJsonCount(self::DAY_COUNT + 1, 'data');
+        $response->assertJsonCount(3, 'table');
+        $response->assertJsonStructure([
+            'keys',
+            'data' => [
+                '*' => [
+                    'date',
+                    'total',
+                ],
+            ],
+            'table' => [
+                '*' => [
+                    'choices',
+                    'count',
+                ],
+            ],
+        ]);
+    }
+}
