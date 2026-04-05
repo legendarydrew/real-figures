24ccc4a73d8fa48b5192dd77671526e7ef26d711
Drew Maughan <drew@pzlabs.co>
2026-04-05 21:58:24 +0100

    feat: updating the Donate page to link to a JustGiving team page, adding support for more charities.

---
+++ resources/views/front/donate/us.blade.php
@@ -0,0 +1,5 @@
+<p>Donations made to us will go toward the costs associated with building and maintaining the site,
+    supporting our Acts and their music production, and aiding the MODE Family in their time of need.
+</p>
+<p>You can also <a href="{{route('contact')}}">contact us</a> directly with other suggestions for supporting
+    the Contest.</p>
