25a7c716c688bf19f5c9ce68cb7bdd0c4f398625
Drew Maughan <drew@pzlabs.co>
2026-04-06 15:32:50 +0100

    feat: adding a rank column to the acts table.

---
+++ app/Enums/ActRank.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+// As suggested by ChatGPT.
+enum ActRank: int
+{
+    case DOMINANT = 0;  // An Act with a large following.
+    case LOVED = 1;     // An Act loved by their fans.
+    case DIVISIVE = 2;  // An Act that splits public opinion.
+    case UNDERDOG = 3;  // An Act that isn't expected to win.
+    case WILDCARD = 4;  // An Act that isn't widely known..
+}
---
+++ database/migrations/2026_04_06_141859_update_acts_table_add_rank_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Enums\ActRank;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('acts', function (Blueprint $table)
+        {
+            $table->integer('rank')->default(ActRank::DIVISIVE->value)->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('acts', function (Blueprint $table)
+        {
+            $table->dropColumn('rank');
+        });
+    }
+};
