3c3728175ef9c813c2e0d02cc4631db90b04f035
Drew Maughan <drew@pzlabs.co>
2026-04-15 15:42:30 +0100

    feat: tinkering with Acts page text.

--- resources/views/front/acts.blade.php
+++ resources/views/front/acts.blade.php
@@ -9,7 +9,11 @@
 
     <div class="site-container">
         <h1 class="page-heading">Competing Acts</h1>
-        <p>Meet the 32 Acts that were shortlisted for CATAWOL Records' Real Figures Don't F.O.L.D Song Contest.</p>
+        <p>
+            Meet the 32 Acts that were shortlisted for CATAWOL Records' Real Figures Don't F.O.L.D Song Contest.
+            Your votes determine which Acts wil go through to the Final, and which Song will be chosen as the
+            official anthem of the Contest.
+        </p>
 
         @if(count($acts))
             <div class="acts-grid">
