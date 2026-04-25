078574ca0716a458b3cc4569ecfd1111b52d450e
Drew Maughan <drew@pzlabs.co>
2026-04-25 10:05:14 +0100

    feat: double shuffle when alocating Songs to Rounds.

--- app/Services/RoundAllocate.php
+++ app/Services/RoundAllocate.php
@@ -17,46 +17,62 @@ class RoundAllocate
      * Create one or more Rounds for a Stage, allocating the specified Songs to each Round.
      * This will also remove any existing Rounds.
      *
-     * @throws Exception
+     * @throws Exception|\Throwable
      */
-    public function songs(Stage $stage, Collection $songs, ?int $songs_per_round = null, ?Carbon $round_start = null, string $round_duration = '6 days'): void
+    public function songs(Stage      $stage,
+                          Collection $songs,
+                          ?int       $songs_per_round = null,
+                          ?Carbon    $round_start = null,
+                          string     $round_duration = '7 days'): void
     {
-        if ($songs->count() === 0) {
+        if ($songs->isEmpty())
+        {
             throw new DataException('No Songs were provided.');
-        } elseif ($songs->count() < 2) {
+        }
+        elseif ($songs->count() < 2)
+        {
             throw new DataException('At least two Songs are required.');
-        } elseif ($stage->hasStarted()) {
+        }
+        elseif ($stage->hasStarted())
+        {
             throw new DataException('Stage has already started.');
         }
 
-        if ($songs_per_round > 1) {
-            $songs = $songs->shuffle();
+        // Split Songs into Rounds.
+        if ($songs_per_round > 1)
+        {
+            $songs       = $songs->shuffle()->shuffle();
             $song_chunks = $songs->chunk($songs_per_round, false);
-        } else {
+        }
+        else
+        {
             $song_chunks = [$songs];
         }
 
         $round_start = $round_start?->clone() ?? Carbon::now()->addDay()->startOfHour();
 
-        DB::transaction(function () use ($stage, $song_chunks, $round_start, $round_duration) {
+        DB::transaction(function () use ($stage, $song_chunks, $round_start, $round_duration)
+        {
             // Remove all existing Rounds.
             Round::whereStageId($stage->id)->delete();
 
             $interval = \DateInterval::createFromDateString($round_duration);
-            foreach ($song_chunks as $index => $song_chunk) {
+            foreach ($song_chunks as $index => $song_chunk)
+            {
                 $round = Round::factory()->create([
-                    'stage_id' => $stage->id,
-                    'title' => count($song_chunks) === 1 ? 'Final Round' : 'Round '.($index + 1),
+                    'stage_id'  => $stage->id,
+                    'title'     => count($song_chunks) === 1 ? 'Final Round' : 'Round ' . ($index + 1),
                     'starts_at' => $round_start,
-                    'ends_at' => $round_start->clone()->add($interval),
+                    'ends_at'   => $round_start->clone()->add($interval),
                 ]);
-                foreach ($song_chunk as $song) {
+                foreach ($song_chunk as $song)
+                {
                     RoundSongs::create([
                         'round_id' => $round->id,
-                        'song_id' => $song->id,
+                        'song_id'  => $song->id,
                     ]);
                 }
-                $round_start->add($interval)->addMinutes(30);
+                $round_start->add($interval);
             }
         });
     }
